7d2facd8c5e3756f5e3bdc918bb0bad990e294ec
white <whitesuffer602@gmail.com>
2026-09-20 22:02:17 +0300

    Add functions in main for step 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,29 @@
 package org.example;
 
+import org.example.database.Database;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.math.BigDecimal;
 import java.io.IOException;
 import java.sql.SQLException;
 
 @SpringBootApplication
 public class Main {
+
     public static void main(String[] args) throws IOException, SQLException {
-      SpringApplication.run(Main.class, args);
+
+        ApplicationContext context = SpringApplication.run(Main.class, args);
+
+        Database database = context.getBean(Database.class);
+
+        System.out.println(database.getBalance(1));
+
+        database.putMoney(1, new BigDecimal("100"));
+        System.out.println(database.getBalance(1));
+
+        database.takeMoney(1, new BigDecimal("100"));
+        System.out.println(database.getBalance(1));
     }
 }
